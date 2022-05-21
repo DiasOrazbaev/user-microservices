ee04400864b23448932ba16b163239e483435469
Dias <orazbaevdev@gmail.com>
2022-05-22 00:25:19 +0600

    Append fraud microservice

---
+++ fraud/src/main/java/kz/fastjava/fraud/FraudApplication.java
@@ -0,0 +1,11 @@
+package kz.fastjava.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FraudApplication.class, args);
+    }
+}
